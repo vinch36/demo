f2a75c00999a0bb5185cc48c92801e8fd9f92db9
vinch36 <vinch36@wanadoo.fr>
2022-10-04 21:03:40 +0200

    Initialization of demo project maven

---
+++ src/main/java/org/vinch36/App.java
@@ -0,0 +1,13 @@
+package org.vinch36;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
